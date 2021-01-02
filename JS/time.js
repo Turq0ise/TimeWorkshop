6ae88a6862d5198a347fd933f53a25f1acc5a50c
Turq0ise <72288865+Turq0ise@users.noreply.github.com>
2021-01-02 17:58:40 +0800

    Some optimizations, I guess?

--- JS/time.js
+++ JS/time.js
@@ -10,7 +10,7 @@ function mainClock() {
     setTimeout(mainClock, 1000);
 }
 
-function updateTime(n) { /* For minutes and seconds */
+function updateTime(n) {
     if(n < 10) {
         return "0" + n;
     }
@@ -20,27 +20,27 @@ function updateTime(n) { /* For minutes and seconds */
 }
 
 function updateHours(n) {
-    if(n < 10 && n != 0) { //Returns 1am to 9am
+    if(n < 10 && n != 0) {
         midday = "AM";
         return "0" + n;
     }
-    else if(n == 10 || n == 11) { //Returns 10am and 11am
+    else if(n == 10 || n == 11) {
         midday = "AM";
         return n;
     }
-    else if(n == 12) { //Returns 12pm
+    else if(n == 12) {
         midday = "PM";
         return n;
     }
-    else if(n > 12 && n < 22) { //Returns 1pm to 9pm
+    else if(n > 12 && n < 22) {
         midday = "PM";
         return "0" + n % 12;
     }
-    else if(n == 22 || n == 23) { //Returns 10pm and 11pm
+    else if(n == 22 || n == 23) {
         midday = "PM";
         return n % 12;
     }
-    else if(n == 0) { //Returns 12am
+    else if(n == 0) {
         midday = "AM"
         return n = 12;
     }
